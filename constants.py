8da3cc49a0f793c0429160d2eef7302ee6c149d9
Jason <jasonkwok375@gmail.com>
2025-03-25 09:33:12 -0700

    Removed unused stuff

--- constants.py
+++ constants.py
@@ -2,22 +2,15 @@ import math
 
 g = 9.81
 max_time = 2 # [min]
-dt = 0.01/10 # [s]
+dt = 0.01 # [s]
 rho = 1.2
-
-curve_eff = 0.7
-straight_eff = 0.5
-
-curve_accel = 0
 
 max_torque = 8.647/1000
 
 min_height = 7 #cm
 
 timeout = 10
-
-slip_threshold = 0.01 #m
-
+slip_threshold = 0.1 #m
 
 
 class Cargo:
@@ -29,12 +22,9 @@ class Motor:
     torque_s = 0.01729
     k = torque_s/(speed_nl/60*2*math.pi)
 
-    
+# Unused   
 class sim_status:
     COMPLETE = 0
     TIMEOUT = 1
     SLIP = 2
     RUNTIME = 3
-
-
-
